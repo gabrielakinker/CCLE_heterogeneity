3c40f5fbd4b84a81094fb6caae255785506d3645
gabrielakinker <66314122+gabrielakinker@users.noreply.github.com>
2021-02-23 12:35:47 -0300

    Update packages.R

--- packages.R
+++ packages.R
@@ -1,2 +1,2 @@
-install.packages(c("NMF", "Rtsne", "dbscan", "mclust", "RColorBrewer", "viridis", "ggplot2". "scales", "egg", "reshape2"))
+install.packages(c("NMF", "Rtsne", "dbscan", "mclust", "RColorBrewer", "viridis", "ggplot2", "scales", "egg", "reshape2"))
 
